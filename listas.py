0d0f27712ddd9a71c3b1970350fcd1adbb0dff05
Wesley Portugal <wesleyportugal250@gmail.com>
2026-01-07 23:15:53 -0300

    Listas

---
+++ listas.py
@@ -0,0 +1,37 @@
+from usuario import Usuario
+
+
+continuar = 1
+lista_usuarios = []
+
+while continuar != 0:
+    nome = input("Digite o seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    sobrenome = input("Digite seu sobrenome: ")
+    usuario = Usuario(nome, idade, sobrenome)
+
+    lista_usuarios.append(usuario)
+
+    if usuario.idade == 99:
+        break
+    
+    if usuario.idade == 98:
+        continue
+
+    print(f"Olá, {usuario.nome} {usuario.sobrenome}, sua idade é {usuario.idade}")
+
+    if usuario.idade <= 17:
+        print(f"{usuario.nome} é adolescente")
+    elif usuario.idade >= 18 and usuario.idade <= 50:
+        print(f"{usuario.nome} é adulto")
+    else:
+        print(f"{usuario.nome} é idoso")
+
+    continuar = int(input("Deseja continuar cadastrando? 0 - Sair 1 - Continuar"))
+else:
+    print("Lista de usuários cadastrados: ")
+
+    for x in lista_usuarios:
+        print(f"Nome completo: {x.nome} {x.sobrenome} - Idade {x.idade}")
+
+    print("O loop entrou no else")
